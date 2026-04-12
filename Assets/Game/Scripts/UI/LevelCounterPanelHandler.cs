26c9b5c2484f7dcfb1a133ea33017967d9c15f48
Metin Kaim <metinkaim0@gmail.com>
2026-04-12 17:05:42 +0300

    Added level counter text

---
+++ Assets/Game/Scripts/UI/LevelCounterPanelHandler.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+using Assets.Game.Scripts.Signals;
+using Assets.Game.Scripts.Datas.DataValues;
+
+namespace Assets.Game.Scripts.UI
+{
+    public class LevelCounterPanelHandler : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI levelCounterText;
+
+        private void OnEnable()
+        {
+            if (LevelSignals.Instance == null)
+                return;
+
+            LevelSignals.Instance.onLevelLoaded += OnLevelLoaded;
+        }
+
+        private void OnDisable()
+        {
+            if (LevelSignals.Instance == null)
+                return;
+
+            LevelSignals.Instance.onLevelLoaded -= OnLevelLoaded;
+        }
+
+        private void OnLevelLoaded(LevelData levelData)
+        {
+            levelCounterText.text = "Level " + (SaveSignals.Instance.onGetSavedLevelIndex());
+        }
+    }
+}
